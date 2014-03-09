86173c1298d0103ede3551b5f66dceca1353328a
シュール <shule517@gmail.com>
2014-03-09 15:30:23 +0900

    ウィンドウサーチ機能を追加

--- src/ShulePlus/ShulePlusForm.Designer.cs
+++ src/ShulePlus/ShulePlusForm.Designer.cs
@@ -28,21 +28,46 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.textBoxWindowTitle = new System.Windows.Forms.TextBox();
+			this.buttonSearchWindow = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// textBoxWindowTitle
+			// 
+			this.textBoxWindowTitle.Location = new System.Drawing.Point(12, 12);
+			this.textBoxWindowTitle.Name = "textBoxWindowTitle";
+			this.textBoxWindowTitle.Size = new System.Drawing.Size(260, 19);
+			this.textBoxWindowTitle.TabIndex = 0;
+			// 
+			// buttonSearchWindow
+			// 
+			this.buttonSearchWindow.Location = new System.Drawing.Point(12, 37);
+			this.buttonSearchWindow.Name = "buttonSearchWindow";
+			this.buttonSearchWindow.Size = new System.Drawing.Size(84, 23);
+			this.buttonSearchWindow.TabIndex = 1;
+			this.buttonSearchWindow.Text = "ウィンドウ検索開始";
+			this.buttonSearchWindow.UseVisualStyleBackColor = true;
+			this.buttonSearchWindow.MouseDown += new System.Windows.Forms.MouseEventHandler(this.buttonSearchWindow_MouseDown);
 			// 
 			// ShulePlusForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.buttonSearchWindow);
+			this.Controls.Add(this.textBoxWindowTitle);
 			this.Name = "ShulePlusForm";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.ShulePlusForm_Load);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TextBox textBoxWindowTitle;
+		private System.Windows.Forms.Button buttonSearchWindow;
 	}
 }
 
--- src/ShulePlus/ShulePlusForm.cs
+++ src/ShulePlus/ShulePlusForm.cs
@@ -1,9 +1,28 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
 
 namespace ShulePlus
 {
 	public partial class ShulePlusForm : Form
 	{
+		/// <summary>
+		/// ウィンドウサーチ中か
+		/// </summary>
+		bool windowSearching = false;
+
+		/// <summary>
+		/// フック対象のウィンドウハンドル
+		/// </summary>
+		IntPtr hookWindowHandle = IntPtr.Zero;
+
+		/// <summary>
+		/// 現在マウス位置にあるウィンドウハンドル
+		/// </summary>
+		IntPtr nowWindowHandle = IntPtr.Zero;
+
 		public ShulePlusForm()
 		{
 			InitializeComponent();
@@ -11,14 +30,81 @@ namespace ShulePlus
 
 		private void ShulePlusForm_Load(object sender, System.EventArgs e)
 		{
-			WindowHook hook = new WindowHook(this.Handle);
+			MouseHook mouseHook = new MouseHook();
+			mouseHook.MouseHooked += (_sender, _e) =>
+			{
+				if (!windowSearching)
+				{
+					return;
+				}
+
+				if (_e.Message == MouseMessage.LUp)
+				{
+					hookWindowHandle = nowWindowHandle;
+					HookWindow(hookWindowHandle);
+					windowSearching = false;
+				}
+
+				if (_e.Message == MouseMessage.Move)
+				{
+					IntPtr handle = GetWindowHandle();
+					nowWindowHandle = handle;
+
+					StringBuilder str = new StringBuilder(256);
+					GetWindowText(handle, str, 256);
+					textBoxWindowTitle.Text = str.ToString();
+				}
+			};
+		}
+
+		private void HookWindow(IntPtr handle)
+		{
+			// ウィンドウフック
+			WindowHook hook = new WindowHook(handle);
 			hook.WindowProc += (_sender, _e) =>
 			{
 				Message message = (_e as WindowProcEventArgs).Message;
 				WindowsMessage winmessage = (WindowsMessage)message.Msg;
-
 				System.Console.WriteLine("Msg : {0}\t{1}\t{2}", winmessage, message.WParam, message.LParam);
 			};
+		}
+
+		private IntPtr GetWindowHandle()
+		{
+			// マウスポジションの取得
+			Rectangle screen = Screen.GetBounds(this);
+			int mouseX = Control.MousePosition.X;
+			int mouseY = Control.MousePosition.Y;
+
+			POINT bottomPoint;
+			bottomPoint.x = mouseX;
+			bottomPoint.y = mouseY;
+
+			IntPtr handle = WindowFromPoint(bottomPoint);
+			return handle;
+		}
+
+		/// <summary>
+		/// 座標を含むウインドウのハンドルを取得
+		/// </summary>
+		/// <param name="Point">調査する座標</param>
+		/// <returns>ポイントにウインドウがなければNULL</returns>
+		[DllImport("user32.dll")]
+		public static extern IntPtr WindowFromPoint(POINT Point);
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct POINT
+		{
+			public int x;
+			public int y;
+		}
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+		private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+		private void buttonSearchWindow_MouseDown(object sender, MouseEventArgs e)
+		{
+			windowSearching = true;
 		}
 	}
 }
