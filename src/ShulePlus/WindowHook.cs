e7085a3a012e23c35ca4b50b65b93185a99e834f
シュール <shule517@gmail.com>
2014-03-09 13:36:06 +0900

    ウィンドウフッククラスの追加

---
+++ src/ShulePlus/WindowHook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ShulePlus
+{
+	/// <summary>
+	/// ウィンドウフッククラス
+	/// </summary>
+	class WindowHook : NativeWindow
+	{
+		/// <summary>
+		/// フック対象のウィンドウハンドルを設定
+		/// </summary>
+		/// <param name="handle">フック対象のウィンドウハンドル</param>
+		public WindowHook(IntPtr handle)
+		{
+			// サブクラスウィンドウの設定
+			AssignHandle(handle);
+		}
+
+		/// <summary>
+		/// フック対象のウィンドウプロシージャ
+		/// </summary>
+		/// <param name="m"></param>
+		protected override void WndProc(ref Message m)
+		{
+			base.WndProc(ref m);
+		}
+	}
+}
